6c4efea12d107430c3e99b3910c1d01b03d31da0
Xulin Li <47105200+kw66@users.noreply.github.com>
2025-09-10 17:57:11 +0800

    Update uniat.py

--- atreid/model/uniat.py
+++ atreid/model/uniat.py
@@ -14,7 +14,7 @@ def weights_init(m):
 
 class uniat(nn.Module):
     def __init__(self, num_p, num_c, imsize=(256, 128), drop=0.2, stride=16, moe=True):
-        super(vitmoe, self).__init__()
+        super(uniat, self).__init__()
         model_path = "/home/lixulin/.cache/torch/checkpoints/jx_vit_base_p16_224-80ecf9dd.pth"
         dim = 768
         self.base = vit_base_patch16_224_ReID_moe(
