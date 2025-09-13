b1d8233611d9f58231b4a3c583c3dd9becc3afd2
Xulin Li <47105200+kw66@users.noreply.github.com>
2025-09-13 20:27:31 +0800

    Add files via upload

---
+++ atreid/others/test_all.py
@@ -0,0 +1,118 @@
+import time
+from others.eval import eval, extract_feature
+from torch.nn import functional as F
+import numpy as np
+from torch.utils.data import DataLoader
+from dataset.datamanager import ImageDataset
+from others.transforms import get_transform
+import torch
+
+
+def test0(args, d='market', dataset=None, model=None):
+    start = time.time()
+    t = 1
+    if d in ['sysu', 'sysu_indoor', 'sysu_multi', 'sysu_indoor_multi',
+             'regdb_v2i', 'regdb_i2v', 'llcm_v2i', 'llcm_i2v']:
+        mode1 = 'cm'
+    else:
+        mode1 = 'vm'
+    if d in ['prcc', 'ltcc', 'deepchange', 'vc']:
+        mode2 = 'cc'
+    elif d in ['prcc_sc', 'vc_sc']:
+        mode2 = 'sc'
+    else:
+        mode2 = 'all'
+    if d in ['sysu', 'sysu_indoor', 'sysu_multi', 'sysu_indoor_multi',
+             'llcm_v2i', 'llcm_i2v']:
+        t = 10
+    for trial in range(t):
+        if d in ['market', 'cuhk', 'msmt', 'deepchange', 'ltcc_all', 'vc', 'vc_sc']:
+            query = np.array(dataset.query)
+            gallery = np.array(dataset.gallery)
+        if d == 'sysu':
+            query = np.array(dataset.query)
+            gallery = np.array(dataset.gallery_1[trial])
+        if d == 'sysu_indoor':
+            query = np.array(dataset.query)
+            gallery = np.array(dataset.gallery_i_1[trial])
+        if d == 'sysu_multi':
+            query = np.array(dataset.query)
+            gallery = np.array(dataset.gallery_10[trial])
+        if d == 'sysu_indoor_multi':
+            query = np.array(dataset.query)
+            gallery = np.array(dataset.gallery_i_10[trial])
+        if d == 'regdb_v2i':
+            query = np.array(dataset.test_ir[args.trial])
+            gallery = np.array(dataset.test_rgb[args.trial])
+        if d == 'llcm_v2i':
+            query = np.array(dataset.q_ir)
+            gallery = np.array(dataset.g_rgb[trial])
+        if d == 'regdb_i2v':
+            query = np.array(dataset.test_rgb[args.trial])
+            gallery = np.array(dataset.test_ir[args.trial])
+        if d == 'llcm_i2v':
+            query = np.array(dataset.q_rgb)
+            gallery = np.array(dataset.g_ir[trial])
+        if d == 'prcc':
+            query = np.array(dataset.query_cc)
+            gallery = np.array(dataset.gallery)
+        if d == 'prcc_sc':
+            query = np.array(dataset.query_sc)
+            gallery = np.array(dataset.gallery)
+        if d == 'ltcc':
+            query = np.array(dataset.query_cc)
+            gallery = np.array(dataset.gallery_cc)
+        transform_train, transform_test = get_transform(args)
+        queryloader = DataLoader(
+            ImageDataset(query, transform=transform_test),
+            batch_size=args.test_batch, num_workers=args.workers,
+            pin_memory=True, drop_last=False, )
+        galleryloader = DataLoader(
+            ImageDataset(gallery, transform=transform_test),
+            batch_size=args.test_batch, num_workers=args.workers,
+            pin_memory=True, drop_last=False, )
+        q, q_pids, q_cids, q_mids, q_camids = extract_feature(model, queryloader, args.flip)
+        g, g_pids, g_cids, g_mids, g_camids = extract_feature(model, galleryloader, args.flip)
+        distmat = 1 - torch.mm(F.normalize(q, p=2, dim=1), F.normalize(g, p=2, dim=1).t()).numpy()
+        cmc_t, mAP_t = eval(q, q_pids, q_cids, q_mids, q_camids, g, g_pids,
+                            g_cids, g_mids, g_camids, mode1, mode2, distmat, d)
+        if trial == 0:
+            cmc, mAP = cmc_t / t, mAP_t / t
+        else:
+            cmc, mAP = cmc + cmc_t / t, mAP + mAP_t / t
+    print(f'{mAP * 100:-2.2f}\t{cmc[0] * 100:-2.2f}\t{cmc[4] * 100:-2.2f}\t{cmc[9] * 100:-2.2f}\t{cmc[19] * 100:-2.2f}')
+    print(f'Evaluation Time:\t {time.time() - start:.3f}')
+    return cmc, mAP
+
+
+def test(args, dataset=None, model=None):
+    if args.d == 'sysu':
+        _, _ = test0(args, d='sysu_rgb', dataset=dataset, model=model)
+        _, _ = test0(args, d='sysu_ir', dataset=dataset, model=model)
+        _, _ = test0(args, d='sysu_multi', dataset=dataset, model=model)
+        _, _ = test0(args, d='sysu_indoor', dataset=dataset, model=model)
+        _, _ = test0(args, d='sysu_indoor_multi', dataset=dataset, model=model)
+        cmc, mAP = test0(args, d='sysu', dataset=dataset, model=model)
+    elif args.d == 'llcm':
+        cmc1, mAP1 = test0(args, d='llcm_v2i', dataset=dataset, model=model)
+        cmc2, mAP2 = test0(args, d='llcm_i2v', dataset=dataset, model=model)
+        cmc = cmc1/2+cmc2/2
+        mAP = mAP1/2+mAP2/2
+    elif args.d == 'regdb':
+        cmc1, mAP1 = test0(args, d='regdb_v2i', dataset=dataset, model=model)
+        cmc2, mAP2 = test0(args, d='regdb_i2v', dataset=dataset, model=model)
+        cmc = cmc1 / 2 + cmc2 / 2
+        mAP = mAP1 / 2 + mAP2 / 2
+    elif args.d == 'prcc':
+        _, _ = test0(args, d='prcc_sc', dataset=dataset, model=model)
+        cmc, mAP = test0(args, d='prcc', dataset=dataset, model=model)
+    elif args.d == 'ltcc':
+        _, _ = test0(args, d='ltcc_all', dataset=dataset, model=model)
+        cmc, mAP = test0(args, d='ltcc', dataset=dataset, model=model)
+    elif args.d == 'vc':
+        _, _ = test0(args, d='vc_sc', dataset=dataset, model=model)
+        cmc, mAP = test0(args, d='vc', dataset=dataset, model=model)
+    else:
+        cmc, mAP = test0(args, d=args.d, dataset=dataset, model=model)
+    return cmc, mAP
+
